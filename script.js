c3428b05647a58c4854ba3f32f6b903ea8a46418
Claudiazepam <martinezallendesclaudia@gmail.com>
2024-07-14 22:52:17 -0400

    actividad 4 completa

---
+++ script.js
@@ -0,0 +1,19 @@
+/*info1 = prompt("Ingresa una direccion")
+info2 = prompt("Ingresa una comuna")
+
+ ele1 = document.querySelector(".info1")
+ ele2 = document.querySelector(".info2")
+
+//info1.innerHTML(ele1)
+//info2.innerHTML(ele2)
+ele1.innerHTML = info1
+ele2.innerHTML = info2*/
+
+img = prompt("Ingresa un borde")
+borde = document.querySelector("img")
+img.style.borderRadius = borde
+
+
+
+
+
